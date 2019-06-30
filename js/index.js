91ba96e2e49b210ee221ff965ce935d458d65208
ilya <napastyuk@live.ru>
2019-06-30 20:38:31 +0300

    clean code

--- js/index.js
+++ js/index.js
@@ -1,36 +1,42 @@
 (function init() {
 
+    /*configuration options*/
+    let citySelectWrapperEl = document.querySelector('.citySelectWrapper');
+    let clockEl = document.querySelector('.clock');
+    let amPmSwitchEl = document.querySelector('#amPmFormatSwitch');
+    let dateEl = document.querySelector('.date');
+    let cityListEl = document.querySelector('.citySelectWrapper');
+
     let syncInterval = 5 * 60 * 1000; // как часто синхронизировать с внешним API в миллисекундах
-    let updatedTime = new Date(); //время обновленное через интернет или локально инкрементом по секундам  
-    const cityList = {
+    let updatedTime = new Date(); //время обновленное через интернет или локально инкрементом по секундам 
+    let syncAPIurl = 'http://worldtimeapi.org/api/timezone/Europe/Moscow';
+    const cityList = {            //массив городов и названий часовых поясов по IANA
         LED: {
             name: 'Санкт-Петербург, Россия',
-            timezoneName: 'Europe/Moscow'  //имя часовой зоны по IANA
+            timezoneName: 'Europe/Moscow'
         },
         JFK: {
             name: 'Нью-Йорк, США',
-            timezoneName: 'America/New_York'  //имя часовой зоны по IANA
+            timezoneName: 'America/New_York'
         },
         NRT: {
             name: 'Токио, Япония',
-            timezoneName: 'Asia/Tokyo'  //имя часовой зоны по IANA 
+            timezoneName: 'Asia/Tokyo'
         }
     }
-    let cityListSelectedItem = cityList.LED;
 
     function startClock() {
         updateTime(); //сразу запускаем setInterval callback что бы браузер не ждал одну секунду до первого обновления
         syncTime();  //сразу запускаем синхронизацию, на случай если на клиенте установлено неправильное время 
-        let timerId = setInterval(updateTime, 1000);
-        let syncId = setInterval(syncTime, syncInterval);
+        setInterval(updateTime, 1000);
+        setInterval(syncTime, syncInterval);
     }
 
     function syncTime() {
-        sendXMLHttpRequest('http://worldtimeapi.org/api/timezone/Europe/Moscow');
+        sendXMLHttpRequest(syncAPIurl);
     }
 
     function sendXMLHttpRequest(url) {
-        console.log("отправляем запрос на " + url);
         let xhr = new XMLHttpRequest();
         xhr.open("GET", url, true);
         xhr.onreadystatechange = function () {
@@ -38,6 +44,7 @@
                 parseNewTime(xhr.responseText);
             } else if (this.status == 0) {
                 console.log('Отсутствует подключение к интернету');
+                updatedTime = new Date(); //если не получилось синхронизироватся с интернетом, синхронизируется с локальными часами
             } else if (this.status != 200) {
                 console.error('Ошибка. Статус: ' + this.status + ' Этап запроса ' + this.readyState);
             }
@@ -47,10 +54,12 @@
 
     function parseNewTime(newTimeStr) {
         let newTimeObj = JSON.parse(newTimeStr);
-        updatedTime = new Date(newTimeObj.datetime);
+        //при создании new Date информация о часовом поясе все равно потеряется 
+        //поэтому используем поле datetime
+        updatedTime = new Date(newTimeObj.datetime); 
     }
 
-    function updateTime() {
+    function updateTime() {   //локальное обновление времени
         updatedTime.setSeconds(updatedTime.getSeconds() + 1);
         showFormattedTime();
     }
@@ -60,44 +69,49 @@
     /*--------------*/
 
     function renderView() {
-        
+        //сгенерируем селект из тех городов которые есть в объекте cityList        
+        let citySelectLabelEl = document.createElement('label');
+        citySelectLabelEl.setAttribute('for', 'citySelect');
+        citySelectLabelEl.innerText = 'Выберете город: ';
+        citySelectWrapperEl.appendChild(citySelectLabelEl);
+
+        let citySelectSelectEl = document.createElement('select');
+        citySelectSelectEl.setAttribute('id', 'citySelect');
+        for (let prop in cityList) {
+            let citySelectOptionEl = document.createElement('option');
+            citySelectOptionEl.setAttribute('value', prop);
+            citySelectOptionEl.innerText = cityList[prop].name;
+            citySelectSelectEl.appendChild(citySelectOptionEl);
+        }
+        citySelectWrapperEl.appendChild(citySelectSelectEl);
+
+        //запускаем приложение
         startClock();
     }
 
     function showFormattedTime() {
-        let clockEl = document.querySelector('.clock');
-        let amPmFormatSwitchEl = document.querySelector('#amPmFormatSwitch');
-        let cityNameEl = document.querySelector('.cityName');
-        let dateEl = document.querySelector('.date');
-        let cityListSelectEl = document.querySelector('.citySelect > #citySelect');
-        let cityListSelectedItem = Object.keys(cityList)[cityListSelectEl.selectedIndex];
-        // if (cityListSelectedItem!="LED") debugger;
+        let cityListSelectorEl = cityListEl.querySelector('#citySelect');
+        let selectedItem = Object.keys(cityList)[cityListSelectorEl.selectedIndex];
+        let selectedTimezone = cityList[selectedItem].timezoneName;
 
         let clockOptions = {
-            hour12: amPmFormatSwitchEl.checked,
+            hour12: amPmSwitchEl.checked,
             hour: '2-digit',
             minute: '2-digit',
-            second: '2-digit', 
-            timeZone: cityList[cityListSelectedItem].timezoneName
+            second: '2-digit',
+            timeZone: selectedTimezone
         };
         let dateOptions = {
-            weekday:'long',
-            year:'numeric',
-            month:'long',
-            day:'numeric',
-            timeZone: cityList[cityListSelectedItem].timezoneName
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            timeZone: selectedTimezone
         };
-    
+
         if (toLocaleStringSupportsLocales() != true) console.log = ('Браузер не поддерживает toLocaleString()');
-
-        clockEl.innerText = updatedTime.toLocaleString('ru-RU', clockOptions);
-        
-        cityNameEl.innerText = cityList[cityListSelectedItem].name + " - точное время";
-        
+        clockEl.innerText = document.title = updatedTime.toLocaleString('ru-RU', clockOptions);
         dateEl.innerText = updatedTime.toLocaleString('ru-RU', dateOptions);
-
-        //document.title = updatedTime.toLocaleString('ru-RU', options);
-        // https://jsfiddle.net/yashva/9fuj3pad/
     }
 
     function toLocaleStringSupportsLocales() {
